994fc7eb58b7b49784d308348d10f0b4b969636d
rai-kriti <149457879+rai-kriti@users.noreply.github.com>
2025-05-25 01:56:38 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,23 +1,22 @@
 self.addEventListener("install", (event) => {
-    event.waitUntil(
-      caches.open("static").then((cache) => {
-        return cache.addAll([
-          "./",
-          "./index.html",
-          "./style.css",
-          "./app.js",
-          "./manifest.json",
-          "./img.png"
-        ]);
-      })
-    );
-  });
-  
-  self.addEventListener("fetch", (event) => {
-    event.respondWith(
-      caches.match(event.request).then((response) => {
-        return response || fetch(event.request);
-      })
-    );
-  });
-  
+  event.waitUntil(
+    caches.open("static").then((cache) => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./style.css",
+        "./app.js",
+        "./manifest.json",
+        "./img.png"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
